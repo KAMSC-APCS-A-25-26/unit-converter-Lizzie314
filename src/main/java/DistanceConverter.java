af76d8a01cdc24ed6fcb20c7778b178ad2e3c00b
Lizzie314 <elizabethbippley@gmail.com>
2025-09-29 13:55:58 -0400

    Update DistanceConverter.java

--- src/main/java/DistanceConverter.java
+++ src/main/java/DistanceConverter.java
@@ -4,13 +4,6 @@ public class DistanceConverter
 {
     public static void main(String[] args)
     {
-        // TODO: Implement distance converter
-        // Requirements:
-        // - Menu with options: 1. Convert Miles to Kilometers, 2. Convert Kilometers to Miles, 3. Exit
-        // - Loop until user chooses to exit
-        // - Use conversion factor: 1 mile = 1.60935 kilometers
-        // - Display conversion results
-        // - Handle invalid menu choices
         Scanner sc = new Scanner(System.in);
         boolean run = true;
         while (run)
